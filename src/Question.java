edce6c4010de7f9d32c5d59a4337a634bdd7c61b
Joshua Boyd <jaboydfamily@gmail.com>
2021-12-12 22:20:25 -0500

    can now successfully create and access question objects

--- src/Question.java
+++ src/Question.java
@@ -6,20 +6,43 @@ class Question {
 	private String quesText;
 	private ArrayList<String> quesAns = new ArrayList<String>();
 
-	Question(int qIdIn, int qTypeIn, String quesTxtIn, ArrayList<String> quesAnsIn) {	
+	Question() {
 
+	}
+
+	Question(int qIdIn, int qTypeIn, String quesTxtIn, ArrayList<String> quesAnsIn) {	
+		this.qId = qIdIn;
+		this.qType = qTypeIn;
+		this.quesText = quesTxtIn;
+		this.quesAns = quesAnsIn;
+	}
+
+	public void setQId(int qIdIn) {
+		this.qId = qIdIn;
 	}
 
 	public int getQId() {
 		return this.qId;
 	}
 
+	public void setQType(int qTypeIn) {
+		this.qType = qTypeIn;
+	}
+
 	public int getQType() {
 		return this.qType;
 	}
 
+	public void setQuesText(String quesTxtIn) {
+		this.quesText = quesTxtIn;
+	}
+
 	public String getQuesText() {
 		return this.quesText;
+	}
+
+	public void setQuesAns(ArrayList<String> quesAnsIn) {
+		this.quesAns = quesAnsIn;
 	}
 
 	public ArrayList<String> getQuesAns() {
