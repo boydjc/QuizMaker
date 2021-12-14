ed05b47b092a7ed38d255bd8a4624751fcff2733
Joshua Boyd <jaboydfamily@gmail.com>
2021-12-14 14:23:12 -0500

    added subpanels and label for question selection

--- src/QuizGui.java
+++ src/QuizGui.java
@@ -4,20 +4,44 @@ import java.awt.*;
 public class QuizGui extends JFrame {
 
 	private JPanel mainPanel = new JPanel();
+
 	private JPanel qSelectDetailPanel = new JPanel();
-	private JPanel previewPanel = new JPanel();
+	private JPanel qSelectPanel = new JPanel();
+	private JLabel qSelectLabel = new JLabel("Question Banks");
+
+	private JPanel previewPanel = new JPanel();;
 
 
 	QuizGui() {
+
+		// JFrame configuration
 		super("QuizMaker");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
 		setLocation(60, 60);
 		setLayout(new FlowLayout());
 		
+		// set layouts for panels
 		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
+
 		qSelectDetailPanel.setLayout(new BoxLayout(qSelectDetailPanel, BoxLayout.LINE_AXIS));
+		qSelectPanel.setLayout(new BoxLayout(qSelectPanel, BoxLayout.PAGE_AXIS));
+		
 		previewPanel.setLayout(new BoxLayout(previewPanel, BoxLayout.PAGE_AXIS));
+
+		// qSelectDetailPanel components and subpanels
+
+		// qSelectPanel components and subpanels
+		qSelectPanel.add(qSelectLabel);
+		// end qSelectPanel components and subpanels
+		qSelectDetailPanel.add(qSelectPanel);
+
+		// end qSelectDetailPanel components and sub panels
+		mainPanel.add(qSelectDetailPanel);
+
+		mainPanel.add(previewPanel);
+
+		add(mainPanel);
 
 		setSize(600, 480);
 	}
