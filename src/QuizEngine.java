5b0df6c9235c3612e44cc889eba4a06eac592ab4
Joshua Boyd <jaboydfamily@gmail.com>
2021-12-16 13:36:50 -0500

    removed all MySQL implementations in preparation to fully embrace data serialization system

--- src/QuizEngine.java
+++ src/QuizEngine.java
@@ -1,11 +1,7 @@
 import java.util.ArrayList;
 import java.util.Random;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class QuizEngine {
-	
-	private DbConnection dbConn;
 
 	// set of all the questions we have in the selected table
 	private ArrayList<Question> questionSet;
@@ -15,14 +11,7 @@ public class QuizEngine {
 
 	private Random rand = new Random();
 
-	QuizEngine(String dbNameIn, String dbPortIn, String dbUserIn, String dbPassIn) {
-		dbConn = new DbConnection(dbNameIn, dbPortIn, dbUserIn, dbPassIn);
-	}
-
-	public void setConnectionDetails(String dbNameIn, String dbPortIn,
-									 String dbUserIn, String dbPassIn) {
-		dbConn.setConnectionDetails(dbNameIn, dbPortIn,
-									dbUserIn, dbPassIn);
+	QuizEngine() {
 	}
 
 	public void setQuestionSet(ArrayList<Question> qSetIn) {
@@ -54,79 +43,6 @@ public class QuizEngine {
 			quizSet.add(questionSet.get(qSequence.get(i)));
 		}
 		System.out.println("Quiz Generated Successfully");
-	}
-
-	public void getAllQuestions(String tableName) {
-
-		dbConn.makeConnection();
-
-		dbConn.executeStatement("SELECT * FROM " + tableName);
-
-		questionSet = new ArrayList<Question>();
-		
-		ResultSet rs = dbConn.getResultSet();
-
-		try {
-			while(rs.next()) {
-				/* rs.getString(1) qId
-				/ rs.getString(2) qType
-				/ rs.getString(3) qText
-				/ rs.getString(4) choiceOne
-				/ rs.getString(5) choiceTwo
-				/ rs.getString(6) choiceThree
-				/ rs.getString(7) choiceFour
-				/ rs.getString(8) choiceFive
-				/ rs.getString(9) choiceSix
-				/ rs.getString(10) choiceSeven
-				/ rs.getString(11) choiceEight
-				/ rs.getString(12) choiceNine
-				/ rs.getString(13) choiceTen
-				/ rs.getString(14) ansOne
-				/ rs.getString(15) ansTwo
-				/ rs.getString(16) ansThree
-				/ rs.getString(17) ansFour
-				/ rs.getString(18) ansFive
-				/ rs.getString(19) ansSix
-				/ rs.getString(20) ansSeven
-				/ rs.getString(21) ansEight
-				/ rs.getString(22) ansNine
-				/ rs.getString(23) ansTen	*/
-
-				ArrayList<String> choices = new ArrayList<String>();
-				ArrayList<String> answers = new ArrayList<String>();
-
-				// go through the choices and only add them is they are not blank
-				for(int i=4; i<=13; i++) {
-					if(rs.getString(i) != null) {
-						choices.add(rs.getString(i));
-					}
-				}
-
-				// same with the answers
-				for(int i=14; i<=23; i++) {
-					if(rs.getString(i) != null) {
-						answers.add(rs.getString(i));
-					}
-				}
-
-				// create new question
-				Question q = new Question(rs.getInt(1),
-										  rs.getInt(2),
-										  rs.getString(3),
-										  choices,
-										  answers);
-				
-				// add the question to our questionSet
-				questionSet.add(q);
-
-			} 
-		} catch (SQLException e) {
-			System.out.println("SQLException: " + e.getMessage());
-		}
-
-		dbConn.closeConnection();
-
-		System.out.println("Questions for " + tableName + " retrieved.");
 	}
 
 	// generates a random array of numbers 1 - x
