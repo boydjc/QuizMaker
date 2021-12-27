2aedfeae81212e1627f0ca0a8e82dc4380fb1959
Joshua Boyd <jaboydfamily@gmail.com>
2021-12-26 21:30:40 -0500

    question number and question text being displayed on quiz screen

--- src/QuizEngine.java
+++ src/QuizEngine.java
@@ -7,9 +7,11 @@ public class QuizEngine {
 	private ArrayList<Question> questionSet;
 
 	// set of questions used for the current quiz
-	private ArrayList<Question> quizSet;
+	private ArrayList<Question> generatedQuizSet;
 
 	private Random rand = new Random();
+
+	private int genQuizCurQuesNum = 1;
 
 	QuizEngine() {
 	}
@@ -23,25 +25,47 @@ public class QuizEngine {
 	}
 
 	public ArrayList<Question> getQuizSet() {
-		return this.quizSet;
+		return this.generatedQuizSet;
 	}
 
+	// prints all of the questions, mostly for testing purposes
 	public void displayQuiz() {
-		for(int i=0; i<quizSet.size(); i++) {
-			System.out.println(quizSet.get(i).getQuesText());
+		for(int i=0; i<generatedQuizSet.size(); i++) {
+			System.out.println(generatedQuizSet.get(i).getQuesText());
 		}
+	}
+	
+	// returns a question from the generated quiz
+	public Question getQuestion(int qIndex) {
+		return generatedQuizSet.get(qIndex);
+	}
+
+	public void incrementCurQuesNum() {
+		genQuizCurQuesNum++;
+	}
+
+	public void decrementCurQuesNum() {
+		genQuizCurQuesNum--;
+	}
+	
+	// returns the number of the question from the currently
+	// generated quiz
+	public int getCurQuesNum() {
+		return genQuizCurQuesNum;	
 	}
 
 	public void generateQuiz() {
 
 		// clear the current quiz if there is one
-		quizSet = new ArrayList<Question>();
+		generatedQuizSet = new ArrayList<Question>();
 
 		ArrayList<Integer> qSequence = getRandQuesSeq(questionSet.size());
 		
 		for(int i=0; i<questionSet.size(); i++) {
-			quizSet.add(questionSet.get(qSequence.get(i)));
+			generatedQuizSet.add(questionSet.get(qSequence.get(i)));
 		}
+
+		genQuizCurQuesNum = 1;
 		System.out.println("Quiz Generated Successfully");
 	}
 
