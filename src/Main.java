0666aea144e29accaf44d291cf2f4590e5315bc9
Joshua Boyd <jaboydfamily@gmail.com>
2022-01-05 11:00:53 -0500

    program will now check for data dir and create if not exist

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
-import java.util.ArrayList;
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
-import java.io.IOException;
+import java.io.File;
 
 public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Running...");
+
+		// check that the data directory exists, if not then create it
+		File file = new File("." + File.separator + "data");
+		if(!(file.exists())) {
+			file.mkdir();
+		}
 
 		QuizGui qGui = new QuizGui();
 
